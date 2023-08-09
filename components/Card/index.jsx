887abd27db40decdb86d5c40b35b05c38f87e5df
aniketSingh0310 <aniketreuls@gmail.com>
2023-08-09 21:26:24 +0530

    videos

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -5,7 +5,9 @@ import cls from "classnames";
 import { motion } from "framer-motion";
 
 const Card = (props) => {
-    const { imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2059&q=80", size = "medium" } = props;
+    const { imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2059&q=80", 
+    size = "medium", 
+    id } = props;
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
   const classMap = {
@@ -13,16 +15,17 @@ const Card = (props) => {
     medium: styles.mdItem,
     small: styles.smItem,
   };
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
   const handleOnError = () => {
     console.log("hii error");
     setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2059&q=80");
   };
     return ( 
         <div className={styles.container}>
-            Card
+            
             <motion.div
         className={cls(styles.imgMotionWrapper, classMap[size])}
-        whileHover={{ scale: 1.2 }}
+        whileHover={{ ...scale }}
       >
         <Image
           src={imgSrc}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
-import Banner from '@/components/Banner'
-import Card from '@/components/Card'
-import Navbar from '@/components/Navbar'
-import Head from 'next/head'
-import Image from 'next/image'
+import Banner from "@/components/Banner";
+import SectionCards from "@/components/Card/section-card";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const disneyVideos = [
+    {
+      imgUrl: "/static/barbie.webp",
+    },
+    {
+      imgUrl: "/static/batman.jpg",
+    },
+    {
+      imgUrl: "/static/Barbie2.webp",
+    },
+  ];
   return (
-    
     <div>
-     <Head>
-      <title>Netflix Clone</title>
-     </Head>
-      <Navbar username="aniket@gmail.com"/>
-      <Banner title="The Batman" subTitle="Gotham Series" imgUrl="/static/batman.jpg"/>
-      <Card imgUrl="/static/barbie.webp" size="large"/>
-      <Card  />
-      <Card imgUrl="/static/Barbie2.webp" size="small"/>
-
-   </div>
-  )
+      <Head>
+        <title>Netflix Clone</title>
+      </Head>
+      <Navbar username="aniket@gmail.com" />
+      <Banner
+        title="The Batman"
+        subTitle="Gotham Series"
+        imgUrl="/static/batman.jpg"
+      />
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+      </div>
+    </div>
+  );
 }
