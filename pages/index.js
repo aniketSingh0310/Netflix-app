329fb84e6f321cb04c1f6a3dad04266ad74c802c
aniketSingh0310 <aniketreuls@gmail.com>
2023-08-10 13:39:27 +0530

    feat:youtubeAPI additions

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,56 @@ import SectionCards from "@/components/Card/section-card";
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { getVideos } from "@/lib/video";
+import { getVideos,getPopularVideos } from "@/lib/video";
 
 
+
+// export async function getServerSideProps(context) {
+//   const disneyVideos = await getVideos();
+
+//   return {
+//     props: { disneyVideos }, // will be passed to the page component as props
+//   };
+// }
 
 export async function getServerSideProps(context) {
-  const disneyVideos = getVideos();
-
-  return {
-    props: { disneyVideos }, // will be passed to the page component as props
-  };
+  try {
+  const disneyVideos = await getVideos("disney trailer");
+  const marvelVideos = await getVideos("marvel trailer");
+  const footballVideos = await getVideos("Champions league football");
+  const popularVideos = await getPopularVideos();
+  
+    return {
+      props: { disneyVideos,marvelVideos,footballVideos,popularVideos },
+    };
+  } catch (error) {
+    console.error('Error fetching videos:', error);
+    return {
+      props: { disneyVideos: [] }, // Provide an empty array as a fallback
+    };
+  }
 }
-
-export default function Home({disneyVideos}) {
+export default function Home({disneyVideos,marvelVideos,footballVideos,popularVideos}) {
   return (
     <div>
       <Head>
         <title>Netflix Clone</title>
       </Head>
+      <div className={styles.main}>
+
       <Navbar username="aniket@gmail.com" />
       <Banner
         title="The Batman"
         subTitle="Gotham Series"
         imgUrl="/static/batman.jpg"
-      />
+        />
       <div className={styles.sectionWrapper}>
         <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+        <SectionCards title="Marvel" videos={marvelVideos} size="medium" />
+        <SectionCards title="Football" videos={footballVideos} size="small" />
+        <SectionCards title="Popular" videos={popularVideos} size="medium" />
       </div>
+        </div>
     </div>
   );
 }
